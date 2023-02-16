039bfd7b45e69f3503bd61bb8e556ce18e3f775c
Wughanga Rose <wughangar.rose@gmail.com>
2023-02-16 09:27:51 -0800

    four

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,2 +1,12 @@
-#!/bin/bash
-#include<stdio.h>int main(){puts("Programming is like building a multilingual puzzle");return 0;}
+#include <stdio.h>
+
+/**
+ * main - Prints a sentence to stdout.
+ *
+ * Return: Always 0 (success).
+ */
+int main(void)
+{
+    puts("\"Programming is like building a multilingual puzzle");
+    return (0);
+}
