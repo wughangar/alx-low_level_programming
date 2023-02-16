56562f40eaaf046e1b6e3b29d91205a262b9b248
Wughanga Rose <wughangar.rose@gmail.com>
2023-02-16 11:08:10 -0800

    five

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,7 +1,11 @@
 #include <stdio.h>
-
+/**
+ * main-prints a sentence to stdout
+ *
+ * Return: Alwats 0 (success)
+ */
 int main(void)
 {
-    printf("with proper grammar, but the outcome is a piece of art,\n");
-    return (0);
+printf("with proper grammar, but the outcome is a piece of art,\n");
+return (0);
 }
