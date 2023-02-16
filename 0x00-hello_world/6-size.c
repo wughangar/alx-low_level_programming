2b98113e590342bc7f55de4fa8e7cdd2e15f6b46
Wughanga Rose <wughangar.rose@gmail.com>
2023-02-16 11:22:53 -0800

    six

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,14 +1,18 @@
 #include <stdio.h>
-
+/**
+ * main-Prints a sentence to stdout
+ *
+ * Return:Always 0 (success)
+ */
 int main(void)
 {
-    printf("Size of a char: %ld byte(s)\n", sizeof(char));
-    printf("Size of an int: %ld byte(s)\n", sizeof(int));
-    printf("Size of a long int: %ld byte(s)\n", sizeof(long int));
-    printf("Size of a long long int: %ld byte(s)\n", sizeof(long long int));
-    printf("Size of a float: %ld byte(s)\n", sizeof(float));
-    printf("Size of a double: %ld byte(s)\n", sizeof(double));
-    printf("Size of a long double: %ld byte(s)\n", sizeof(long double));
+printf("Size of a char: %ld byte(s)\n", sizeof(char));
+printf("Size of an int: %ld byte(s)\n", sizeof(int));
+printf("Size of a long int: %ld byte(s)\n", sizeof(long int));
+printf("Size of a long long int: %ld byte(s)\n", sizeof(long long int));
+printf("Size of a float: %ld byte(s)\n", sizeof(float));
+printf("Size of a double: %ld byte(s)\n", sizeof(double));
+printf("Size of a long double: %ld byte(s)\n", sizeof(long double));
 
-    return 0;
+return (0);
 }
