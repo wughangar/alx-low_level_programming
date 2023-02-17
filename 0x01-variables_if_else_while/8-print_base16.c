06a1485fe9a1a38bb09a41b90c92c8d306e5ee4a
Wughanga Rose <wughangar.rose@gmail.com>
2023-02-17 05:44:01 -0800

    eight

--- 0x01-variables_if_else_while/8-print_base16.c
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -1,20 +1,25 @@
 #include <stdio.h>
+
 /**
- * main- displays on stdout
+ * main - Entry point
  *
- * Return: Always 0 (success)
+ * Return: Always 0 (Success)
  */
 int main(void)
 {
 char c;
-for (c = '0'; c <= '9'; c++);
+
+for (c = '0'; c <= '9'; c++)
 {
 putchar(c);
 }
-for (c = 'a'; c <= 'f'; c++);
+
+for (c = 'a'; c <= 'f'; c++)
 {
-putchar(c)
+putchar(c);
 }
+
 putchar('\n');
+
 return (0);
 }
