2bb20d5aba2d7058663f82ff523f70e6a2f594f3
Wughanga Rose <wughangar.rose@gmail.com>
2023-02-21 11:43:31 -0800

    6

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,12 @@
+#include "main.h"
+#include <math.h>
+
+/** 
+ * _abs - compute the absolute value of the specified number
+ * @n: the number to compute the absolute value of
+ * Return: the absolute of the number
+ */
+int _abs(int num)
+{
+	return (abs(num));
+}
