8b77ff51286512fddadd6cbb38f7cd603a80f125
Wughanga Rose <wughangar.rose@gmail.com>
2023-02-21 11:02:00 -0800

    3

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -1,22 +1,7 @@
 #include "main.h"
-/**
- * main - prints output
- * 
- * Return: Always 0.
- */
 
-int _islower(int c);
+int _islower(int c)
+{
+  return c >= 'a' && c <= 'z';
+}
 
-int main(void)
-{
-char c = 'a';
-if (_islower(c))
-{
-_putchar("%c is lowercase\n", c);
-}
-else
-{
-_putchar("%c is not lowercase\n", c);
-}
-return 0;
-}
