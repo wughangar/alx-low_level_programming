d289adb3136e2954af73f6d6a8d83fcbc8e041dc
Wughanga Rose <wughangar.rose@gmail.com>
2023-02-21 10:19:16 -0800

    one

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,17 +1,3 @@
-#include "main.h"
-/**
- * main- check the code
- *
- * Return: Always 0.
- */
-void print_alphabet(void);
-int main(void)
-{
-	print_alphabet();
-	return (0);
-}
-
-
 void print_alphabet(void)
 {
 char letter = 'a';
