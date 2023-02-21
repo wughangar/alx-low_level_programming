2b1a224e2ae191cd119c5bd7d45988fb0726d888
Wughanga Rose <wughangar.rose@gmail.com>
2023-02-21 11:25:03 -0800

    double betty

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -2,7 +2,8 @@
 
 /**
  * _islower - determins if the specified character is lowercase
- * c - the character to test
+ * @c: the character to test
+ * Return: 1 if the character is lowercase, otherwise 0 
 */
 int _islower(int c)
 {
--- 0x02-functions_nested_loops/4-isalpha.c
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -2,15 +2,16 @@
 #include <ctype.h>
 
 /**
- * _isalpha - tests if the specified character is an alpha character 
- * c - the character to test
+ * _isalpha - tests if the specified character is an alpha character
+ * @c: the character to test
+ * Return: 1 if the number is alpha otherwise 0
 */
 int _isalpha(int c)
 {
-       if (isalpha(c) != 0)
-       {
-	       return (1);
-       }
-
-       return (0);
+	if (isalpha(c) != 0)
+	{
+		return (1);
+	}
+	
+	return (0);
 }
