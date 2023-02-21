8bd07195a56894f66271946cfb9019bab2a5feed
Wughanga Rose <wughangar.rose@gmail.com>
2023-02-21 09:48:37 -0800

    one

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,19 +1,25 @@
 #include "main.h"
 /**
- * main- check the code 
+ * main- check the code
  *
- *Return: Always 0.
+ * Return: Always 0.
  */
-void print_alphabet(void);
-
 int main(void)
 {
-	char letter = 'a',
-	while(letter <= 'z');
-	{
-	_putchar(letter);
-	letter++;
-	}
-	_putchar('\n');
+	print_alphabet();
 	return (0);
+}
+
+{
+void print_alphabet(void);
+char letter = 'a';
+
+while (letter <= 'z')
+
+{
+_putchar(letter);
+letter++;
+}
+_putchar('\n');
+return (0);
 }
