f912fe4838acfcb6449e2f2405817830ac600d20
Wughanga Rose <wughangar.rose@gmail.com>
2023-02-21 05:22:01 -0800

    one

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,21 +1,19 @@
 #include "main.h"
-/**
- *main- chcek the code
- *
- *Return: Always 0.
- */
 
-void print_alphabet(void);
+/**
+ * main- prints in stdout
+ *
+ * Return: Always 0.
+ */
 
 int main(void)
 {
-char letter = 'a';
-while
-(letter <= 'z');
-{
-_putchar(letter);
-letter++;
-}
-_putchar('\n');
+	char letter = 'a';
+	while (letter <= 'z')
+	{
+	_putchar(letter);
+	letter++;
+	}
+_putchar('\n)
 return (0);
 }
