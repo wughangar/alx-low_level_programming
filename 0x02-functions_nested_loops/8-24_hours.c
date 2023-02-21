6b14005bcc4762f0fdd042ff6f9ad69466d8f547
Wughanga Rose <wughangar.rose@gmail.com>
2023-02-21 12:20:05 -0800

    dear betty, leave me alone

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -1,12 +1,12 @@
 #include "main.h"
 #include <stdio.h>
 
-/** 
+/**
  * jack_bauer - prints every minute of the day
  */
 void jack_bauer(void)
 {
-	int h =0;
+	int h = 0;
 	int m = 0;
 
 	for (h = 0; h < 24; ++h)
