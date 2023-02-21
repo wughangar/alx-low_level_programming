40844a1c6385a9389136fd07f4c0b46e8ff5600d
Wughanga Rose <wughangar.rose@gmail.com>
2023-02-21 11:15:49 -0800

    4

---
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -0,0 +1,11 @@
+#include "main.h"
+#include <ctype.h>
+
+/**
+ * _isalpha - tests if the specified character is an alpha character 
+ * c - the character to test
+*/
+int _isalpha(int c)
+{
+	return isalpha(c);
+}
