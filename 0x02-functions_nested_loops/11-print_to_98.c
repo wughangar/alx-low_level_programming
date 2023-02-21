9b4cf54b627ed1a47f999447209b1cb0e89ad6bd
Wughanga Rose <wughangar.rose@gmail.com>
2023-02-21 12:54:07 -0800

    fix

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -12,8 +12,8 @@ void print_to_98(int n)
 	if (n > 98)
 	{
 		return;
-	}
-	
+	}	
+
 	for (i = n; i <= 98; ++i)
 	{
 		printf("%d", i);
@@ -24,4 +24,6 @@ void print_to_98(int n)
 		}
 
 	}
+
+	_putchar('\n');
 }
