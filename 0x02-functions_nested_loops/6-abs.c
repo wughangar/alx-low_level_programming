3466c9ca25b7e0b6619383edfdd121886fb4cc36
Wughanga Rose <wughangar.rose@gmail.com>
2023-02-21 11:49:06 -0800

    fix bettty

--- 0x02-functions_nested_loops/6-abs.c
+++ 0x02-functions_nested_loops/6-abs.c
@@ -1,12 +1,12 @@
 #include "main.h"
 #include <stdlib.h>
 
-/** 
+/**
  * _abs - compute the absolute value of the specified number
  * @n: the number to compute the absolute value of
  * Return: the absolute of the number
  */
-int _abs(int num)
+int _abs(int n)
 {
-	return (abs(num));
+	return (abs(n));
 }
