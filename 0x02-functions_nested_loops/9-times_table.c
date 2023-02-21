33f94e0199b75d8f6f5c76da5b86c7eabc09423f
Wughanga Rose <wughangar.rose@gmail.com>
2023-02-21 12:27:19 -0800

    9

---
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -0,0 +1,30 @@
+#include "main.h"
+
+/**
+ * times_table - print the 9 times table
+ */
+void times_table(void)
+{
+	int row = 0;
+	int col = 0;
+
+	for (row = 0; row <= 9; ++row)
+	{
+		for (col = 0; col <=9; ++col)
+		{
+			printf("%-3d", row * col);
+			
+			if (col == 9)
+			{
+				_putchar('$');
+			}
+			else
+			{
+				_putchar(',');
+			}
+		}
+
+		_putchar('\n');
+	}
+}
+
