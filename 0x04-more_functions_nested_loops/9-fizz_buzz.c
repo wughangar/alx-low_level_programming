dc55596b44262df9b164f61e12554358ab120ce5
wughangar <wughangar.rose@gmail.com>
2023-02-23 14:14:36 +0000

    fizz

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -1,14 +1,38 @@
 #include <stdio.h>
+#include "main.h"
 
 /**
- * main - prints the game
- * Return: Always 0 (success)
+ * main - entry point
+ *
+ * Description: prints the numbers 1 - 100 w/
+ * Fizz for multiples of 3, Buzz for multiples of 5,
+ * and FizzBuzz for multiples of both
+ *
+ * Return: always 0
  */
+int main(void)
 {
 	int i;
-	char *fizzbuzz = "FizzBuzz";
-	for (i = 1; i <= 100; i++;)
+
+	for (i = 1; i <= 100; i++)
 	{
-		if (i % 3 == 0 && 1 % 5 == 0)
-		{
+		if (i % 15 == 0)
+			printf("FizzBuzz");
+		else if (i % 3 == 0)
+			printf("Fizz");
+		else if (i % 5 == 0)
+			printf("Buzz");
+		else
+			printf("%i", i);
+		if (i < 100)
+			printf(" ");
+	}
+	printf("\n");
+	return (0);
+}
+Footer
+© 2023 GitHub, Inc.
+Footer navigation
+Terms
+Privacy
 
