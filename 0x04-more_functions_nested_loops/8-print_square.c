3add5bb66b396521419b5606d6ebe4b6f625828f
wughangar <wughangar.rose@gmail.com>
2023-02-23 14:11:51 +0000

    prints a square

--- 0x04-more_functions_nested_loops/8-print_square.c
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -3,19 +3,21 @@
 
 /**
  * print_square - prints a square.
+ * @size: shows square size
  */
 
 void print_square(int size)
 {
-	int size;
-	int i;
-	if (size = 0;)
+	int i, j;
+
+	if (size <= 0)
+		_putchar('\n');
+	for (i = 0; i < size; i++)
 	{
+		for (j = 0; j < (size); j++)
+		{
+			_putchar('#');
+		}
 		_putchar('\n');
 	}
-for (i = 0; i < size; i++;)
-{
-	_putchar('#');
-}
-	_putchar('\n');
 }
