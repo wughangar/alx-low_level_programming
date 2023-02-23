1f4169a1d07eeb9bc3237bbad142a30fddd684f3
wughangar <wughangar.rose@gmail.com>
2023-02-23 08:11:22 +0000

    prints numbers 0-9

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * print_numbers - prints numbers 0-9
+ */
+void print_numbers(void)
+{
+	for(int ch = '0'; ch <= '9'; ch++)
+	{
+		_putchar(ch);
+	}
+	
+	_putchar('\n');
+}
