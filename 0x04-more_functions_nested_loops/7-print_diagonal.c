e7d232149cbc787ebdf754d3959a597f37aba0f8
wughangar <wughangar.rose@gmail.com>
2023-02-23 14:04:33 +0000

    diagonal line

--- 0x04-more_functions_nested_loops/7-print_diagonal.c
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -12,13 +12,14 @@ void print_diagonal(int n)
 
 	if (n <= 0)
 		_putchar('\n');
-
-	for (i = 0; i <= n; i++)
+	for (i = 0; i < n; i++)
 	{
-		for (j = o; j < i; j++)
+		for (j = 0; j < i; j++)
 		{
 			_putchar(' ');
 		}
 		_putchar('\\');
 		_putchar('\n');
 	}
+
+}
