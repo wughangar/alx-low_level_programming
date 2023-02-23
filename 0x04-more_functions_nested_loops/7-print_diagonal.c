db15389293b6677f69a456056c0bf399d8c3312e
wughangar <wughangar.rose@gmail.com>
2023-02-23 08:52:36 +0000

    drawsa a diagnol line

---
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -0,0 +1,21 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * print_diagnol - draws a diagnol line
+ */
+
+void print_diagonal(int n)
+{
+	int n;
+	int i;
+	if (n <= 0)
+	{
+		_putchar('\n');
+	}
+	for (i - 0; i < n; i++;)
+	{
+		_putchar('\');
+	}
+	_putchar('n');
+}
--- 0x04-more_functions_nested_loops/main.h
+++ 0x04-more_functions_nested_loops/main.h
@@ -6,3 +6,5 @@ int _putchar(int ch);
 void print_most_numbers(void);
 void more_numbers(void);
 void print_line(int n);
+void print_diagonal(int n);
+
