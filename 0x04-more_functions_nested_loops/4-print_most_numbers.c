1f352624e660b90e17cf40dbf16ead4035dedb84
wughangar <wughangar.rose@gmail.com>
2023-02-23 14:37:50 +0000

    prints number from 0-9

--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -5,12 +5,13 @@
  */
 void print_numbers(void)
 {
-	char c;
-
-	for (c = '0'; c <= '9'; c++)
+	int i;
+	for (i = 0; i < 10; i++)
 	{
-	if (!(c == '2' || c == '4'))
-	_putchar(c);
+		if (i !=2 && i !=4)
+		{
+			_putchar(i + '0');
+		}
 	}
 	_putchar('\n');
 }
