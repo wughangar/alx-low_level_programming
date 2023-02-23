5b7fe6519d2c18c48cebd91bd6a22794cb5475ec
wughangar <wughangar.rose@gmail.com>
2023-02-23 13:17:13 +0000

    four

--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -5,14 +5,12 @@
  */
 void print_numbers(void)
 {
-	char  c;
+	char c;
 
-	for (c = '0'; ch <= '9'; c++)
+	for (c = '0'; c <= '9'; c++)
 	{
-		if (!(ch == '2' || ch == '4'))
-
-		_putchar(c);
+	if (!(c == '2' || c == '4'))
+	_putchar(c);
 	}
-
 	_putchar('\n');
 }
