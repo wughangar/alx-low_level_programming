cd7dc51f3d23110b0dc8e747574bcdadb249f5bf
wughangar <wughangar.rose@gmail.com>
2023-02-23 07:42:51 +0000

    zero

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,17 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * _isupper - chceks if character is upper case
+ * @c: character to test
+ * Return: 1 if uppercase otherwise 0
+ */
+int _isupper(int c)
+{
+	if (isupper(c))
+	{
+		return (1);
+	}
+
+	return (0);
+}
---
+++ 0x04-more_functions_nested_loops/README.md
@@ -0,0 +1,2 @@
+function that checks for uppercase
+
