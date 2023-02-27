586f76cc9fbf5f7a710e2c6395e4e29d86e86f94
wughangar <wughangar.rose@gmail.com>
2023-02-27 14:02:49 +0000

    function that reverses string

--- 0x05-pointers_arrays_strings/5-rev_string.c
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -4,13 +4,16 @@
 /**
  * rev_string- function to reverse string
  * @s: string to be reversed
- * Return: 0.
  */
 void rev_string(char *s)
 {
-	char s;
+	int len = strlen(s);
+	int i;
+
+	for (i = 0; i < len/2; i++)
 	{
-		_putchar(strrev(s));
+		char temp = s[i];
+		s[i] = s[len-1-i];
+		s[len-1-i] = temp;
 	}
-	return (o);
 }
