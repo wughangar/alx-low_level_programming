61f2fa7a75488731ff3b71ffa4f8120eaf552c51
wughangar <wughangar.rose@gmail.com>
2023-02-27 17:41:19 +0000

    function to print second half of string

---
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,35 @@
+#include "main.h"
+#include <string.h>
+
+/**
+ * puts_half - prints the last half of the string to stdout
+ * @str: string to print
+ */
+void puts_half(char *str)
+{
+	int i, len;
+
+	i = 0;
+	len = 0;
+
+	while (str[len] != '\0')
+	{
+		len++;
+	}
+
+	if (len % 2 == 1)
+	{
+		i = (len - 1) / 2;
+	}
+	else
+	{
+		i = len / 2;
+	}
+
+	for (; i < len; ++i)
+	{
+		_putchar(str[i]);
+	}
+
+	_putchar('\n');
+}
