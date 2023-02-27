d2da6c5438cb6bd85fcecafabe3d8176030019f2
wughangar <wughangar.rose@gmail.com>
2023-02-27 17:12:06 +0000

    correction

--- 0x05-pointers_arrays_strings/main.h
+++ 0x05-pointers_arrays_strings/main.h
@@ -3,6 +3,6 @@ void swap_int(int *a, int *b);
 int _strlen(char *s);
 void _puts(char *str);
 int _putchar(char c);
-void print_rev(char *s)i;
+void print_rev(char *s);
 void rev_string(char *s);
 void puts2(char *str);
