887af0b985a0540e5ca22f990b13fd7b1f3efffc
wughangar <wughangar.rose@gmail.com>
2023-03-07 11:00:34 +0000

    main updated

--- 0x07-pointers_arrays_strings/main.h
+++ 0x07-pointers_arrays_strings/main.h
@@ -1,3 +1,6 @@
+#ifndef MAIN_H
+#define MAIN_H
+
 char *_memset(char *s, char b, unsigned int n);
 int _putchar(char c);
 char *_memcpy(char *dest, char *src, unsigned int n);
@@ -7,3 +10,4 @@ char *_strpbrk(char *s, char *accept);
 void print_chessboard(char (*a)[8]);
 char *_strstr(char *haystack, char *needle);
 void print_diagsums(int *a, int size);
+#endif/*MAIN_H*/
