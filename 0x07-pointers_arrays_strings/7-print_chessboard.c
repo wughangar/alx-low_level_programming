8747965f8ac25fdedfaadd97b5978c3e15d40537
wughangar <wughangar.rose@gmail.com>
2023-03-07 08:48:56 +0000

    change

--- 0x07-pointers_arrays_strings/7-print_chessboard.c
+++ 0x07-pointers_arrays_strings/7-print_chessboard.c
@@ -8,12 +8,16 @@ void print_chessboard(char (*a)[8])
 {
 	int i, j;
 
-	for (i = 0; i < 8; i++)
+	j = 0;
+
+	for (i = 0; i < 64; i++)
 	{
-		for (j = 0; j < 8; j++)
+		if (i % 8 == 0 && i != 0)
 		{
-			_putchar(a[i][j]);
+			j = i;
+			_putchar(10);
 		}
-		_putchar('\n');
+		_putchar(a[i / 8][i - j]);
 	}
+	_putchar(10);
 }
