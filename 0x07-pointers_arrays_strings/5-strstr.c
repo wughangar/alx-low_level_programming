cc0e0a807388dd7d1d6cd6190f447b8d867385ec
wughangar <wughangar.rose@gmail.com>
2023-03-07 10:48:08 +0000

    fice

--- 0x07-pointers_arrays_strings/5-strstr.c
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -8,27 +8,21 @@
  */
 char *_strstr(char *haystack, char *needle)
 {
-	char *haystack_ptr;
-	char *needle_ptr;
+        unsigned int i = 0, j = 0;
 
-	if (!*needle)
-		return (haystack);
-
-	while
-		(*haystack);
-	{
-		haystack_ptr = haystack;
-		needle_ptr = needle;
-
-		while (*needle_ptr && needle_ptr == *haystack_ptr)
-		{
-			needle_ptr++;
-			haystack_ptr++;
+        while (haystack[i])
+        {
+                while (needle[j] && (haystack[i] == needle[0]))
+                {
+                        if (haystack[i + j] == needle[j])
+                                j++;
+                        else
+                                break;
+                }
+                if (needle[j])
+                {
+                        i++;
+                        j = 0;
 		}
-
-		if (*needle_ptr == '\0')
-			return (haystack);
-		haystack++;
 	}
-	return (NULL);
 }
