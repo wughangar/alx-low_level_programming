fedfd126fb63a4da625ae5d67c47068bd10e974d
wughangar <wughangar.rose@gmail.com>
2023-03-08 11:38:42 +0000

    prime number

--- 0x08-recursion/6-is_prime_number.c
+++ 0x08-recursion/6-is_prime_number.c
@@ -8,18 +8,30 @@
  */
 int is_prime_number(int n)
 {
-	int i;
-
 	if (n <= 1)
+	{
+		return 0;
+	}
+	if (n == 2 || n == 3)
+	{
+		return (1);
+	}
+	if (n % 2 == 0 || n % 3 == 0)
 	{
 		return (0);
 	}
-	for (i = 2; i < n; i++)
+
+	int i = 5;
+	int w = 2;
+
+	while (i * i <= n)
 	{
 		if (n % i == 0)
 		{
 			return (0);
 		}
+		i += w;
+		w = 6 - w;
 	}
 	return (1);
 }
