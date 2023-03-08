5c930d1f2eff428893c579f14bf0c9e9eba264d1
wughangar <wughangar.rose@gmail.com>
2023-03-08 10:53:13 +0000

    returns square root of a number

---
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -0,0 +1,20 @@
+#include "main.h"
+/**
+ * _sqrt_recursion - returns natural square root of a number n using recursion
+ * @n: integer
+ */
+int _sqrt_recursion(int n)
+{
+	if (n < 0)
+	{
+		return (-1);
+	}
+	else if (n == 0 || n == 1)
+	{
+		return (n);
+	}
+	else
+	{
+		return (_sqrt_recursion(n, n / 2));
+	}
+}
