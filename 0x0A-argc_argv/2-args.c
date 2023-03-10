5c04dafc63b8978117ea6dfde7b9453311440ced
wughangar <wughangar.rose@gmail.com>
2023-03-10 10:20:35 +0000

    number of arguments

---
+++ 0x0A-argc_argv/2-args.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+ * main - prints all arguments it receives
+ * @argc: number of arguments
+ * @argv: arguments vector
+ *
+ * Return: 0 (success)
+ */
+int main(int argc, char *argv[])
+{
+	(void)argc;
+
+	{
+		printf("%s\n", argc[]);
+	}
+	return (0);
+}
