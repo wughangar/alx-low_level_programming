d3b20a03bfecbcae1febf40935e99eb1540f551a
wughangar <wughangar.rose@gmail.com>
2023-03-10 10:05:03 +0000

    prints number of arguements

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+/**
+ * main - prints number of arguments passed
+ * @argc: number of arguments passed 
+ * @argv: arguments vector
+ *
+ * Return: 0 (success)
+ */
+int main(int argc, char **argv)
+{
+	(void)argv;
+
+	if (argc != -1)
+	{
+		printf("%d\n", argc);
+	}
+	return(0);
+}
