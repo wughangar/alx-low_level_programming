4a438230ae933c0b8e05de0c3515f02360d352cc
wughangar <wughangar.rose@gmail.com>
2023-03-13 06:18:44 +0000

    prints a duplicate pointer

---
+++ 0x0B-malloc_free/1-strdup.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#include "main.h"
+/**
+ * _strdup - returns a pointer to newly allocated space in memory
+ * @str: string
+ *
+ * Return: NULL if insufficient memory or if str = NULL
+ */
+char *_strdup(char *str)
+{
+	char *dup_str;
+	int str_len;
+
+	if (str == NULL)
+	{
+		return (NULL);
+	}
+	str_len = strlen(str);
+
+	dup_str = (char *) malloc((str_len + 1) * sizeof(char));
+	if (dup_str == NULL)
+	{
+		return (NULL);
+	}
+	strcpy(dup_str, str);
+	return (dup_str);
+}
