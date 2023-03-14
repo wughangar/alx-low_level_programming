0a809e1749ee7bfacb16eca1bdc20e0cf9f82b52
wughangar <wughangar.rose@gmail.com>
2023-03-14 06:06:59 +0000

    free grid

--- 0x0B-malloc_free/3-alloc_grid.c
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -23,23 +23,23 @@ int **alloc_grid(int width, int height)
 	{
 		return (NULL);
 	}
+
 	for (i = 0; i < height; i++)
 	{
 		grid[i] = (int *)malloc(width * sizeof(int));
-		if (grid[i] == (NULL))
+		if (grid[i] == NULL)
 		{
-			for
-				(j = 0; j < i; j++);
+			for (j = 0; j < i; j++)
 			{
-			free(grid[j]);
+				free(grid[j]);
+			}
+			free(grid);
+			return (NULL);
 		}
-		free(grid);
-		return (NULL);
-	}
-	for (j = 0; j < width; j++)
-	{
-		grid[i][j] = 0;
+		for (j = 0; j < width; j++)
+		{
+			grid[i][j] = 0;
+		}
 	}
 	return (grid);
 }
-
