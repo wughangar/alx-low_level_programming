73a66576883dc2d6b00bc259dcd811c870bcacbf
wughangar <wughangar.rose@gmail.com>
2023-03-20 16:38:36 +0000

    betty corrections

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -12,19 +12,20 @@
  */
 dog_t *new_dog(char *name, float age, char *owner)
 {
-	dog_t *dog = (dog_t*) malloc(sizeof(dog_t));
+	dog_t *dog = (dog_t *) malloc(sizeof(dog_t));
+
 	if (dog == NULL)
 	{
 		return (NULL);
 	}
-	dog->name = (char*) malloc(strlen(name) + 1);
+	dog->name = (char *) malloc(strlen(name) + 1);
 	if (dog->name == NULL)
 	{
 		free(dog);
 		return (NULL);
 	}
 	strcpy(dog->name, name);
-	dog->owner = (char*) malloc(strlen(owner) + 1);
+	dog->owner = (char *) malloc(strlen(owner) + 1);
 	if (dog->owner == NULL)
 	{
 		free(dog->name);
