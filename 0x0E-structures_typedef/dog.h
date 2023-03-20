7d23bac90a63b1fdf9d7383c18c4dd193ae6a7e8
wughangar <wughangar.rose@gmail.com>
2023-03-20 15:39:33 +0000

    correct name change

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -8,13 +8,12 @@
  *
  */
 
-struct dog
+typedef struct dog
 {
 	char *name;
 	float age;
 	char *owner;
-};
-typedef dog dog_t
+}; dog_t;
 void init_dog(struct dog *d, char *name, float age, char *owner);
 void print_dog(struct dog *d);
 #endif
