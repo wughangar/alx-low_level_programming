d380bd7347fb4f04552c0eb6768c17299ecddba4
wughangar <wughangar.rose@gmail.com>
2023-03-22 06:34:18 +0000

    function that prints name

---
+++ 0x0F-function_pointers/0-print_name.c
@@ -0,0 +1,17 @@
+#include "function_pointers.h"
+
+/**
+ * print_name - prints a name 
+ * @name: name to print
+ * @f: a pointer to a function that prints a single character
+ *
+ */
+void print_name(char *name, void (*f)(char *))
+{
+	int i = 0;
+
+	for (name[i] != '\0'; i++)
+	{
+		f(&name[i]);
+	}
+}
