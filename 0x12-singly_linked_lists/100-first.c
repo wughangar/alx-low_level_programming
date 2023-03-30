aca947bafc6df679f17356038a09999f9fa364c0
wughangar <wughangar.rose@gmail.com>
2023-03-30 11:04:55 +0000

    corrected letter

--- 0x12-singly_linked_lists/100-first.c
+++ 0x12-singly_linked_lists/100-first.c
@@ -1,6 +1,6 @@
 #include <stdio.h>
 
-coid __attribute__((constructor)) before_main()
+void __attribute__((constructor)) before_main()
 {
 	printf("You're beat! and yet, you must allow,\nI bore my house upon my back!\n");
 }
