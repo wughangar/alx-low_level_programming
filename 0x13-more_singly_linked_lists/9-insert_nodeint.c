c9599d4d78bcef07d70decb1122df9c8e6b16635
wughangar <wughangar.rose@gmail.com>
2023-04-04 07:42:41 +0000

    corrected a node

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -33,9 +33,9 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 	if (new_node == NULL)
 		return (NULL);
 
-	new_node-> = n;
+	new_node->n = n;
 	new_node->next = temp_node->next;
-	temp_node-> = new_node;
+	temp_node->next = new_node;
 
 	return (new_node);
 }
