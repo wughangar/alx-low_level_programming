52220377416ee958e3c31f7227267ff28ad21df8
wughangar <wughangar.rose@gmail.com>
2023-04-04 05:33:30 +0000

    prints tital number in list function

---
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -0,0 +1,21 @@
+#include "lists.h"
+#include <stdio.h>
+
+/**
+ * print_listint - prints all elements in the list
+ * @h: pointed to list
+ *
+ * Return: sum of elemenets
+ */
+size_t print_listint(const listint_t *h)
+{
+	size_t total = 0;
+
+	while (h != NULL)
+	{
+		printf("%d\n", h->n);
+		h = h->next;
+		total++;
+	}
+	return (total);
+}
