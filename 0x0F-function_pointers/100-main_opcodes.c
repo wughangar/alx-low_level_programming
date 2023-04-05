406127bdd5ff6bc11ea13fe8e1f1deacbb9c468d
wughangar <wughangar.rose@gmail.com>
2023-04-05 16:54:40 +0000

    corrected

--- 0x0F-function_pointers/100-main_opcodes.c
+++ 0x0F-function_pointers/100-main_opcodes.c
@@ -2,16 +2,15 @@
 #include <stdlib.h>
 
 /**
- * main - prints the opcodes of the main function
+ *main - prints the opcodes of its own main function
  * @argc: number of arguments
  * @argv: array of argument strings
  *
  * Retun: 0 if success
  */
 int main(int argc, char *argv[])
-{
-	int num_bytes, i;
-	unsigned char *ptr;
+	{
+	int i, num_bytes;
 
 	if (argc != 2)
 	{
@@ -25,12 +24,12 @@ int main(int argc, char *argv[])
 		printf("Error\n");
 		return (2);
 	}
-	ptr = (unsigned char *)main;
-	for (i = 0; i < num_bytes; i++)
-	{
-		printf("%02x", *(ptr + i));
-	}
-	print("\n");
 
-	return (0);
+	unsigned char *p = (unsigned char *) main;
+
+	for (i = 0; i < num_bytes - 1; i++)
+		printf("%02x ", *(p + i));
+		printf("%02x\n", *(p + i));
+
+		return (0);
 }
