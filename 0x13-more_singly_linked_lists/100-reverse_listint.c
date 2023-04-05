a7d1947c501eb607820e0036976b7874d24c82c8
wughangar <wughangar.rose@gmail.com>
2023-04-05 15:44:29 +0000

    corrected

--- 0x13-more_singly_linked_lists/100-reverse_listint.c
+++ 0x13-more_singly_linked_lists/100-reverse_listint.c
@@ -14,11 +14,11 @@ listint_t *reverse_listint(listint_t **head)
 	while (*head)
 	{
 		next = (*head)->next;
-		(*head)->next = prev;
-		prev = *head;
+		(*head)->next = previous;
+		previous = *head;
 		*head = next;
 	}
-	*head = prev;
+	*head = previous;
 
 	return (*head);
 }
