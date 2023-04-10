b8338abdf0a49223f094acac08b794767c8bdfdf
wughangar <wughangar.rose@gmail.com>
2023-04-10 06:20:22 +0000

    grammar

--- 0x14-bit_manipulation/2-get_bit.c
+++ 0x14-bit_manipulation/2-get_bit.c
@@ -13,7 +13,7 @@ int get_bit(unsigned long int n, unsigned int index)
 	unsigned long int y;
 	unsigned long int result;
 
-	x_bits = sifeof(unsigned long int) * 8;
+	x_bits = sizeof(unsigned long int) * 8;
 
 	if (index >= x_bits)
 		return (-1);
