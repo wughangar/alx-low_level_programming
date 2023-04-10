503abe516234c04915e6af1826a5783b936e5da2
wughangar <wughangar.rose@gmail.com>
2023-04-10 17:31:00 +0000

    spelling check

--- 0x15-file_io/2-append_text_to_file.c
+++ 0x15-file_io/2-append_text_to_file.c
@@ -30,7 +30,7 @@ int append_text_to_file(const char *filename, char *text_content)
 		return (1);
 	}
 
-	if (access(filename, F_OK | W_OK) == -1)
+	if (access(filename, W_OK) == -1)
 	{
 		return (-1);
 	}
@@ -40,7 +40,7 @@ int append_text_to_file(const char *filename, char *text_content)
 	{
 		return (-1);
 	}
-	len = sizeof(text_content);
+	len = strlen(text_content);
 	wbytes = write(d, text_content, len);
 	if (wbytes == -1)
 	{
