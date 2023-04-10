4e2e025a6492725364ef80380bb69ad7d7cb6ef9
wughangar <wughangar.rose@gmail.com>
2023-04-10 14:54:31 +0000

    program that copies contents to another file, and main file

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -16,10 +16,67 @@
 
 void cp(int argc, char* argv[])
 {
+	int d_from;
+	int d_to;
+	size_t rbyte;
+	size_t wbyte;
+	char total[1024];
+
 	if (argc != 3)
 	{
 		dprintf(STDERR_FILENO, "Usage: %s file_from file_to\n", argv[0]);
 		exit(97);
 	}
+	char* file_from = argv[1];
+	char* file_to = argv[2];
+
+	if (access(argv[2], F_OK) == 0)
+	{
+		if (truncate(argv[2], 0) == -1)
+		{
+			dprintf(STDERR_FILENO, "Error: can't truncate file %s\n", file_to);
+			exit(99);
+		}
+	}
+	d_from = open(file_from, O_RDONLY);
+	if (d_from == -1)
+	{
+		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", file_from);
+		exit(98);
+	}
+	d_to = opne(file_to, O_WRONLY | O_CREAT | O_TRUNC, 0664);
+	if (d_to == -1)
+	{
+		dprintf(STDERR_FILENO, "Error: cant write to file %s\n", file_to);
+		exit(99);
+	}
+
+	while ((rbyte = read(d_from, total, sizeof(total))) > 0)
+	{
+		wbyte = write(d_to, total, rbyte);
+		if (wbyte == -1)
+		{
+			dprintf(STDERR, "Error: cant write to file %s\n", file_to);
+			exit(99);
+		}
+	}
+	if (rbyte == -1)
+	{
+		dprintf(STDERR_FILENO, "Error: cant read file %s\n", file_from);
+		exit(99);
+	}
+	if (close(d_from) == -1)
+	{
+		dprintf(STDERR_FILENO, "Error: cant close d_from %d\n", d_from)
+			exit(100);
+	}
+	if (close(d_to) == -1)
+	{
+		dprintf(STDERR_FILENO, "Error: cant close d_to %d\n", d_to)
+			exit(100);
+	}
+}
+
+
 
 
