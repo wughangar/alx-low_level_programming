9150a5f9d8d6ab8329d9d244b9e8d3d15c5ea815
wughangar <wughangar.rose@gmail.com>
2023-04-10 10:19:41 +0000

    function that reads and prints

---
+++ 0x15-file_io/0-read_textfile.c
@@ -0,0 +1,48 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <fcntl.h>
+#include <stddef.h>
+
+/**
+ * read_textfile - function that reads a text file and prints it 
+ * @filename: pointer to file 
+ * @letter: size 
+ *
+ * Return: 0 if it fails.
+ */
+size_t read_textfile(const char *filename, size_t letters)
+{
+	int x = 0;
+	int y;
+	char *temp;
+	size_t byt_wrote;
+
+	x = letters * sizeof(char);
+
+	if (filename == NULL)
+	{
+		return (0);
+	}
+	y = open(filename, O_RDONLY);
+	if (y == -1)
+	{
+		return (0);
+	}
+
+	temp = malloc(x);
+
+	byt_wrote = write(y, temp, x);
+
+	if (byt_wrote == (size_t)-1)
+	{
+		return (0);
+	}
+
+	free(temp);
+	close(y);
+
+	return (byt_wrote);
+}
+	
