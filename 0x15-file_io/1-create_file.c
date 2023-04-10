63975861fc551082080099cc53563581d3732c38
wughangar <wughangar.rose@gmail.com>
2023-04-10 11:32:32 +0000

    function that creates file

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -14,4 +14,34 @@
  */
 int create_file(const char *filename, char *text_content)
 {
+	int d;
+	size_t wbyte;
+	size_t len;
 
+	if (filename == NULL)
+		return (-1);
+
+	d = open(filename, O_WRONLY | O_CREAT | O_TRUNC, S_IRUSR | S_IWUSR);
+		if (d == -1)
+		{
+			return (-1);
+		}
+	if (text_content != NULL)
+	{
+		len = 0;
+		while (text_content[len] != '\0')
+		{
+			len++;
+		}
+		wbyte = write(d, text_content, len);
+	}
+	close(d);
+	if (wbyte == (size_t)-1)
+	{
+		return (-1);
+	}
+	else
+	{
+		return (1);
+	}
+}
