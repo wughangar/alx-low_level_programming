1cef12604cc59eadc55a8cf952d77fbea1db64a2
wughangar <wughangar.rose@gmail.com>
2023-04-10 10:53:53 +0000

    functions that reads file

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -14,46 +14,43 @@
  */
 size_t read_textfile(const char *filename, size_t letters)
 {
-	int x = 0;
-	int y;
+	int len;
+	int d;
+	size_t rbyte;
+	size_t wbyte;
 	char *temp;
-	size_t byt_wrote;
-	size_t byte_read;
-
-	x = letters * sizeof(char);
 
 	if (filename == NULL)
 	{
 		return (0);
 	}
-	y = open(filename, O_RDONLY);
-	if (y == -1)
+
+	len = letters * (sizeof(char));
+
+	d = open(filename, O_RDONLY);
+	if (d == -1)
 	{
 		return (0);
 	}
-
-	temp = malloc(x);
+	temp = malloc(len);
 	if (temp == NULL)
 	{
 		return (0);
 	}
-	byte_read = read(y, temp, letters);
-	if (byte_read == -1)
-	{
-		free(temp);
-		close(y);
-		return (0);
-	}
 
-	byt_wrote = write(STDOUT_FILENO, temp, byte_read);
-
-	if (byt_wrote == (size_t)-1)
+	rbyte = read(d, temp, len);
+	if (rbyte == (size_t) -1)
 	{
 		return (0);
 	}
 
+	wbyte = write(d, temp, len);
+	if (wbyte == (size_t) -1)
+	{
+		return (0);
+	}
 	free(temp);
-	close(y);
+	close(d);
 
-	return (byt_wrote);
+	return (rbyte);
 }
