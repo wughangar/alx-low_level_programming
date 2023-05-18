6c5cae9f8f2d987264cc4c1d411317b42539c814
wughangar <wughangar.rose@gmail.com>
2023-05-18 12:04:55 +0000

    add node at the edn of the list

---
+++ 0x17-doubly_linked_lists/3-add_dnodeint_end.c
@@ -0,0 +1,41 @@
+#include <stdlib.h>
+#include <string.h>
+#include <stdio.h>
+#include "lists.h"
+
+/**
+ * add_dnodeint_end - adds node to end of lis
+ * @head: double pointer to head node
+ * @n: node to be added
+ *
+ * Return: address of new element
+ */
+
+dlistint_t *add_dnodeint_end(dlistint_t **head, const int n)
+{
+	dlistint_t nnode;
+	dlistint_t *current;
+
+	nnode = malloc(sizeof(dlistint_t));
+
+	if (nnode == NULL)
+	{
+		return (NULL);
+	}
+
+	nnode->n = n;
+	nnode->next = NULL;
+
+	if (*head == NULL)
+	{
+		*head = nnode;
+		return ();
+	}
+
+	current = *head;
+	while (current->next != NULL)
+	{
+		current = current->next;
+	}
+	current->next = nnode;
+}
