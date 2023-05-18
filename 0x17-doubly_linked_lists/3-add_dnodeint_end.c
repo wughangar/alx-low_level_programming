69adb563843852790a7d243bda54fdff965537bc
wughangar <wughangar.rose@gmail.com>
2023-05-18 12:11:08 +0000

    added return statement

--- 0x17-doubly_linked_lists/3-add_dnodeint_end.c
+++ 0x17-doubly_linked_lists/3-add_dnodeint_end.c
@@ -29,7 +29,7 @@ dlistint_t *add_dnodeint_end(dlistint_t **head, const int n)
 	if (*head == NULL)
 	{
 		*head = nnode;
-		return ();
+		return (nnode);
 	}
 
 	current = *head;
@@ -38,4 +38,6 @@ dlistint_t *add_dnodeint_end(dlistint_t **head, const int n)
 		current = current->next;
 	}
 	current->next = nnode;
+
+	return (nnode);
 }
