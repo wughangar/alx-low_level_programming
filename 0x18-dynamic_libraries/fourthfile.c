5fe08c26c37f0b77a44f0c7151477dadbb5125e6
wughangar <wughangar.rose@gmail.com>
2023-05-22 09:45:53 +0000

    betty requirements and recompiled dynamic library

--- 0x18-dynamic_libraries/fourthfile.c
+++ 0x18-dynamic_libraries/fourthfile.c
@@ -3,11 +3,27 @@
 #include <string.h>
 #include <stdlib.h>
 
+/**
+ * _memcpy - function for memcpy
+ * @dest: character
+ * @src: character
+ * @n: unsigned int
+ *
+ * Return: NULL
+ */
 char *_memcpy(char *dest, char *src, unsigned int n)
 {
 	/* TODO: implement function */
 	return (NULL);
 }
+
+/**
+ * _strchr - function for strchr
+ * @s: pointer to character
+ * @c: character
+ *
+ * Return: NULL
+ */
 
 char *_strchr(char *s, char c)
 {
@@ -15,11 +31,27 @@ char *_strchr(char *s, char c)
 	return (NULL);
 }
 
+/**
+ * _strspn - function for strspn
+ * @s: pointer to character
+ * @accept: pointer to accept
+ *
+ * Return: 0
+ */
+
 unsigned int _strspn(char *s, char *accept)
 {
 	/* TODO: impelement code here */
 	return (0);
 }
+
+/**
+ * _strpbrk - function for strpbrk
+ * @s: pointer to character s
+ * @accept: pointer to accept
+ *
+ * Return: NULL
+ */
 
 char *_strpbrk(char *s, char *accept)
 {
@@ -27,6 +59,13 @@ char *_strpbrk(char *s, char *accept)
 	return (NULL);
 }
 
+/**
+ * _strstr - function for str str
+ * @haystack: pointer to haystack
+ * @needle: pointer to needle
+ *
+ * Return: NULL
+ */
 char *_strstr(char *haystack, char *needle)
 {
 	/* TODO: impelement code here */
