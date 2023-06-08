d33b6d4991295c7274c29d0e418ba849ad829565
wughangar <wughangar.rose@gmail.com>
2023-06-08 11:24:59 +0000

    retrives value at given index

---
+++ 0x1A-hash_tables/4-hash_table_get.c
@@ -0,0 +1,30 @@
+#include "hash_tables.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+/**
+ * hash_table_get - retrives value asociated with a key
+ * @ht: hash table 
+ * @key: key im looking for
+ *
+ * Returns: value or NULL if key cant be found
+ */
+
+char *hash_table_get(const hash_table_t *ht, const char *key)
+{
+	unsigned long int index;
+	hash_node_t *tmp;
+
+	index = hash_djb2((const unsigned char *)key) % ht->size;
+	tmp = ht->array[index];
+
+	while (tmp != NULL)
+	{
+		if (strcmp(tmp->key, key) == 0)
+			return (tmp->value);
+		tmp = tmp->next;
+	}
+
+	return (NULL);
+}
