9e60bf5f676bf9b049c6a991dc8603b5a245db07
wughangar <wughangar.rose@gmail.com>
2023-06-09 06:38:56 +0000

    alloacted memeory for array

--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -52,3 +52,12 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 	ht->array[index] = nnode;
 	return (1);
 }
+
+int main(void)
+{
+    hash_table_t *ht;
+
+    ht = hash_table_create(1024);
+    hash_table_set(ht, "betty", "cool");
+    return (EXIT_SUCCESS);
+}
